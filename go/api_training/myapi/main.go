f53ee8c5b70f08d1acef88e4d228e35a2b5d6ef0
tomonakar <tomohisa.nak@gmail.com>
2023-11-13 16:51:33 +0900

    構造体jsonに変換

--- go/api_training/myapi/main.go
+++ go/api_training/myapi/main.go
@@ -1,14 +1,68 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/tomonakar/go_api_training/handlers"
 )
 
 func main() {
+
+	type Comment struct {
+		CommentID int
+		ArticleID int
+		Message   string
+		CreatedAt time.Time
+	}
+
+	type Article struct {
+		ID          int
+		Title       string
+		Contents    string
+		UserName    string
+		NiceNum     int
+		CommentList []Comment
+		CreatedAt   time.Time
+	}
+
+	comment1 := Comment{
+		CommentID: 1,
+		ArticleID: 1,
+		Message:   "test comment1",
+		CreatedAt: time.Now(),
+	}
+
+	comment2 := Comment{
+		CommentID: 2,
+		ArticleID: 2,
+		Message:   "test comment2",
+		CreatedAt: time.Now(),
+	}
+
+	article := Article{
+		ID:          1,
+		Title:       "first article",
+		Contents:    "this is the test article",
+		UserName:    "tomonakar",
+		NiceNum:     0,
+		CommentList: []Comment{comment1, comment2},
+		CreatedAt:   time.Now(),
+	}
+
+	fmt.Printf("%+v\n", article)
+
+	// json.Marshalで構造体をjsonに変換
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s\n", jsonData)
 
 	r := mux.NewRouter()
 
