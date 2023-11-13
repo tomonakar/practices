601224c0b5358cf25bb8c32f8b2a9e2f97465bd8
tomonakar <tomohisa.nak@gmail.com>
2023-11-13 16:54:57 +0900

    タグをつけてjsonキーを指定

--- go/api_training/myapi/main.go
+++ go/api_training/myapi/main.go
@@ -14,20 +14,20 @@ import (
 func main() {
 
 	type Comment struct {
-		CommentID int
-		ArticleID int
-		Message   string
-		CreatedAt time.Time
+		CommentID int       `json:"comment_id"`
+		ArticleID int       `json:"article_id"`
+		Message   string    `json:"message"`
+		CreatedAt time.Time `json:"created_at"`
 	}
 
 	type Article struct {
-		ID          int
-		Title       string
-		Contents    string
-		UserName    string
-		NiceNum     int
-		CommentList []Comment
-		CreatedAt   time.Time
+		ID          int       `json:"id"`
+		Title       string    `json:"title"`
+		Contents    string    `json:"contents"`
+		UserName    string    `json:"user_name"`
+		NiceNum     int       `json:"nice_num"`
+		CommentList []Comment `json:"comment_list"`
+		CreatedAt   time.Time `json:"created_at"`
 	}
 
 	comment1 := Comment{
