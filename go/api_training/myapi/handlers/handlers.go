2401e14db01063799cac77615ecc94965da2cef0
tomonakar <tomohisa.nak@gmail.com>
2023-11-13 13:24:38 +0900

    gorilla/muxを利用したパスパラメータの取得

--- go/api_training/myapi/handlers/handlers.go
+++ go/api_training/myapi/handlers/handlers.go
@@ -4,63 +4,45 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-)
+	"strconv"
 
-func NotAllowResp(w http.ResponseWriter) {
-	http.Error(w, "Invalid method\n", http.StatusMethodNotAllowed)
-}
+	"github.com/gorilla/mux"
+)
 
 // hello ハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world!\n")
-	} else {
-		NotAllowResp(w)
-	}
+	io.WriteString(w, "Hello, world!\n")
 }
 
 // article ハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "post article\n")
-	} else {
-		NotAllowResp(w)
-	}
+	io.WriteString(w, "post article\n")
 }
 
 // articleリスト ハンドラ
 func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List\n")
-	} else {
-		NotAllowResp(w)
-	}
+	io.WriteString(w, "Article List\n")
 }
 
 // article詳細 ハンドラ
 func GetArticleByIdHandler(w http.ResponseWriter, req *http.Request) {
-	// /article/[:id] =>  /article/1
-	id := req.URL.Path[len("/article/"):]
-	if req.Method == http.MethodGet {
-		io.WriteString(w, fmt.Sprintf("Article %s\n", id))
-	} else {
-		NotAllowResp(w)
+	// mux.Vars(req)でパスパラメータを MAP形式で取得できる
+	// 今回はidを取得するので、idをキーにして値を取得する
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid article ID", http.StatusBadRequest)
+		return
 	}
+	resString := fmt.Sprintf("Article ID is %d\n", articleID)
+
+	io.WriteString(w, resString)
 }
 
 // articleいいね ハンドラ
 func ArticleNiceHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Article Nice\n")
-	} else {
-		NotAllowResp(w)
-	}
+	io.WriteString(w, "Article Nice\n")
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "post comment\n")
-	} else {
-		NotAllowResp(w)
-	}
+	io.WriteString(w, "post comment\n")
 }
--- go/api_training/myapi/main.go
+++ go/api_training/myapi/main.go
@@ -12,12 +12,12 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HelloHandler)
-	r.HandleFunc("/article", handlers.PostArticleHandler)
-	r.HandleFunc("/article/list", handlers.GetArticleListHandler)
-	r.HandleFunc("/article/", handlers.GetArticleByIdHandler)
-	r.HandleFunc("/article/nice", handlers.ArticleNiceHandler)
-	r.HandleFunc("/comment", handlers.PostCommentHandler)
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.GetArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.GetArticleByIdHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.ArticleNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
