fb0bade455dee5cfbbf491705ccb2c0ab20ee475
tomonakar <tomohisa.nak@gmail.com>
2023-11-13 13:07:09 +0900

    ルーターをgorilla/mux利用に変更

--- go/api_training/myapi/main.go
+++ go/api_training/myapi/main.go
@@ -4,18 +4,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/tomonakar/go_api_training/handlers"
 )
 
 func main() {
 
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.GetArticleListHandler)
-	http.HandleFunc("/article/", handlers.GetArticleByIdHandler)
-	http.HandleFunc("/article/nice", handlers.ArticleNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.GetArticleListHandler)
+	r.HandleFunc("/article/", handlers.GetArticleByIdHandler)
+	r.HandleFunc("/article/nice", handlers.ArticleNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
