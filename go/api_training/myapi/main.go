b2cfe9f82450aa4cf58df1523c894f9d0e0c052a
tomonakar <tomohisa.nak@gmail.com>
2023-11-27 19:31:22 +0900

    6.2 サービス層を改修しコントローラ層を導入

--- go/api_training/myapi/main.go
+++ go/api_training/myapi/main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gorilla/mux"
-	"github.com/tomonakar/go_api_training/handlers"
+	"github.com/tomonakar/go_api_training/controllers"
+	"github.com/tomonakar/go_api_training/services"
+)
+
+var (
+	dbUser     = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbDatabase = os.Getenv("DB_NAME")
+	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 )
 
 func main() {
+	// サーバ全体で使用するsql.DBを１つ生成
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("fail to connect db")
+		return
+	}
 
+	// サーバ全体で使用するサービスを１つ生成
+	ser := services.NewMyAppService(db)
+	// サーバ全体で使用するコントローラーを１つ生成
+	con := controllers.NewMyAppController(ser)
+
+	// コントローラとルーティングを紐付け
 	r := mux.NewRouter()
-
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
