e810c862aeb1d26f449fbb8875306f58b0836bf1
tomonakar <tomohisa.nak@gmail.com>
2023-11-28 10:08:46 +0900

    6.3 ルーターの抽象化

--- go/api_training/myapi/main.go
+++ go/api_training/myapi/main.go
@@ -9,8 +9,8 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 
-	"github.com/gorilla/mux"
 	"github.com/tomonakar/go_api_training/controllers"
+	"github.com/tomonakar/go_api_training/routers"
 	"github.com/tomonakar/go_api_training/services"
 )
 
@@ -35,13 +35,7 @@ func main() {
 	con := controllers.NewMyAppController(ser)
 
 	// コントローラとルーティングを紐付け
-	r := mux.NewRouter()
-	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+	r := routers.NewRouter(con)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
