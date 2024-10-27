90ccc43a7623d89e8c23b03d03dd6db0536cefc3
tomonakar <tomohisa.nak@gmail.com>
2024-10-27 20:55:57 +0900

    Document Widgetを表示

--- flutter/dart_patterns_decords/lib/main.dart
+++ flutter/dart_patterns_decords/lib/main.dart
@@ -30,6 +30,7 @@ class DocumentScreen extends StatelessWidget {
     // パターンを使ってレコードを分解して取得している
     // :modified は modified: modifiedの省略形、ローカル変数名を変更する場合は modified: localModified で変更できる
     var (title, :modified) = document.getMetadata();
+    var blocks = document.getBlocks();
 
     return Scaffold(
       appBar: AppBar(
@@ -37,11 +38,40 @@ class DocumentScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Center(
-            child: Text('Last modified $modified'),
-          ),
+          Text('Last modified $modified'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          )
         ],
       ),
+    );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({required this.block, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+      case 'p' || 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+      case _:
+        textStyle = Theme.of(context).textTheme.bodySmall;
+    }
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(block.text, style: textStyle),
     );
   }
 }
