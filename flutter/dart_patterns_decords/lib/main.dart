da3389a1ff95ca06f38917e4f9dc7b719f162d87
tomonakar <tomohisa.nak@gmail.com>
2024-10-27 19:43:21 +0900

    record型のメソッドを追加

--- flutter/dart_patterns_decords/lib/main.dart
+++ flutter/dart_patterns_decords/lib/main.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'data.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const DocumentApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+  const DocumentScreen({required this.document, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var metadataRecord = document.getMetadata();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(metadataRecord.$1),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text('Last modified ${metadataRecord.modified}'),
+          ),
+        ],
       ),
     );
   }
