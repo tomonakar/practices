ee3e9bbf0a64bf68f80a221325a4583c4fe37454
tomonakar <tomohisa.nak@gmail.com>
2024-10-27 20:13:32 +0900

    パターンを利用してデータを抽出

--- flutter/dart_patterns_decords/lib/data.dart
+++ flutter/dart_patterns_decords/lib/data.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 class Document {
   final Map<String, Object?> _json;
@@ -7,12 +8,68 @@ class Document {
   // レコードを返すメソッド
   // Stringは無名のフィールド $1 としてアクセスできる
   // DateTimeは modified としてアクセスできる
-  (String, {DateTime modified}) getMetadata() {
-    var title = "My Document";
-    var now = DateTime.now();
+  // (String, {DateTime modified}) getMetadata() {
+  //   var title = "My Document";
+  //   var now = DateTime.now();
 
-    // かっこで２つの値を囲無事でレコードを構築して返す
-    return (title, modified: now);
+  // かっこで２つの値を囲無事でレコードを構築して返す
+  // return (title, modified: now);
+  // }
+
+  // パターンを利用せずにJSONを読み取るメソッド
+  (String, {DateTime modified}) getMetadataWithoutPattern() {
+    if (_json.containsKey('metadata')) {
+      var metadataJson = _json['metadata'];
+      if (metadataJson is Map) {
+        var title = metadataJson['title'] as String;
+        var localModified = DateTime.parse(metadataJson['modified'] as String);
+        return (title, modified: localModified);
+      }
+    }
+    throw const FormatException('Unexpected JSON');
+  }
+
+  // パターンを使ってJSONを読み取るメソッド
+  (String, {DateTime modified}) getMetadata() {
+    // if-caseを使ってJSONを読み取る
+    if (_json
+        case {
+          'metadata': {
+            'title': String title,
+            'modified': String localModified,
+          }
+        }) {
+      // if-caseで比較したパターンがマッチした場合に実行される
+      return (title, modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
+    }
+  }
+
+  // Blockのリストを返すメソッド
+  List<Block> getBlocks() {
+    // if case を使って JSON の blocks　を blocksJson　として取得
+    if (_json case {'blocks': List blocksJson}) {
+      return <Block>[
+        for (var blockJson in blocksJson) Block.fromJson(blockJson)
+      ];
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
+  }
+}
+
+class Block {
+  final String type;
+  final String text;
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json case {'type': var type, 'text': var text}) {
+      return Block(type, text);
+    } else {
+      throw const FormatException('Unexpected JSON format');
+    }
   }
 }
 
