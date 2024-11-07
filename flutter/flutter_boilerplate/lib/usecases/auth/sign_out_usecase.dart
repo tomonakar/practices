34642162b33a668c6658030c006730692d73e123
tomonakar <tomohisa.nak@gmail.com>
2024-11-08 08:52:13 +0900

    fix: 不要なproviderを削除

--- flutter/flutter_boilerplate/lib/usecases/auth/sign_out_usecase.dart
+++ flutter/flutter_boilerplate/lib/usecases/auth/sign_out_usecase.dart
@@ -1,9 +1,6 @@
-// lib/domain/usecases/auth/sign_out_usecase.dart
 import 'package:flutter_boilerplate/domain/common/result.dart';
 import 'package:flutter_boilerplate/domain/repositories/auth/auth_repository.dart';
 import 'package:flutter_boilerplate/usecases/base/usecase.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 class SignOutUseCase implements UseCase<void, NoParams> {
   final AuthRepository _repository;
@@ -14,10 +11,4 @@ class SignOutUseCase implements UseCase<void, NoParams> {
   Future<Result<void>> execute(NoParams params) {
     return _repository.signOut();
   }
-}
-
-// lib/usecases/providers/usecase_providers.dart に追加
-@riverpod
-SignOutUseCase signOutUseCase(Ref ref) {
-  return SignOutUseCase(ref.watch(authRepositoryProvider));
 }
